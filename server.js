104662e548d09d9ec406a1200682a86f50104a8f
jessedegroothva <jesse.de.groot2@hva.nl>
2023-04-18 12:43:08 +0200

    stijling

--- server.js
+++ server.js
@@ -2,44 +2,45 @@
 https://socket.io/get-started/chat
 */
 
-const express = require('express')
-const app = express()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
-const path = require('path')
-const port = process.env.PORT || 4242
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const path = require("path");
+const port = process.env.PORT || 4242;
 
 // Start het longpolling proces, geef io mee
 // setInterval(callApi, 2500, io)
 
-const historySize = 50
-let history = []
+const historySize = 50;
+let history = [];
 
 // Serveer client-side bestanden
-app.use(express.static(path.resolve('public')))
+app.use(express.static(path.resolve("public")));
 
-io.on('connection', (socket) => {
-  console.log('a user connected')
-  io.emit('history', history)
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  io.emit("history", history);
 
-  socket.on('message', (message) => {
+  socket.on("message", (message) => {
     while (history.length > historySize) {
-      history.shift()
+      history.shift();
     }
-    history.push(message)
+    history.push(message);
 
-    io.emit('message', message)
-  })
+    console.log("Message:", message);
+    io.emit("message", message);
+  });
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected')
-  })
-})
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 function callApi(io) {
-  io.emit('whatever', 'somebody set up us the bomb!')
+  io.emit("whatever", "somebody set up us the bomb!");
 }
 
 http.listen(port, () => {
-  console.log('listening on http://localhost:' + port)
-})
+  console.log("listening on http://localhost:" + port);
+});
