6f23eecf8c6acb2d5ca02150400d8107aa5f5e35
jessedegroothva <jesse.de.groot2@hva.nl>
2023-05-12 14:22:32 +0200

    update

--- public/script.js
+++ public/script.js
@@ -1,24 +1,22 @@
 const socket = io({
   autoConnect: false,
 });
-const messages = document.querySelector("section ul");
+const messages = document.querySelector("#messages");
 const input = document.querySelector("input");
 
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
   if (input.value) {
-    socket.emit("message", input.value);
+    socket.emit("message", { user: socket.auth.user, text: input.value });
     input.value = "";
   }
 });
 
-// aciveer je het
 socket.on("userConnected", (name) => {
-  console.log(name);
   const joinMessage = document.createElement("li");
   joinMessage.classList.add("gameMsg");
 
-  joinMessage.innerText = name;
+  joinMessage.innerText = `${name} joined the chat`;
   messages.appendChild(joinMessage);
 });
 
@@ -26,8 +24,42 @@ socket.on("message", (message) => {
   addMessage(message);
 });
 
-socket.on("whatever", (message) => {
-  addMessage(message);
+function addMessage(message) {
+  // create a new list item
+  const li = document.createElement("li");
+  // add the username and text to the list item
+  li.textContent = `${message.user}: ${message.text}`;
+  // append the list item to the messages list
+  messages.appendChild(li);
+  // scroll to the bottom
+  messages.scrollTop = messages.scrollHeight;
+
+  // save the message in local storage
+  let history = JSON.parse(localStorage.getItem("chatHistory")) || [];
+  if (
+    !history.some(
+      (storedMessage) =>
+        storedMessage.text === message.text &&
+        storedMessage.user === message.user
+    )
+  ) {
+    history.push(message);
+    localStorage.setItem("chatHistory", JSON.stringify(history));
+  }
+}
+
+window.addEventListener("load", () => {
+  // load the history from local storage
+  let history = JSON.parse(localStorage.getItem("chatHistory")) || [];
+  history.forEach(addMessage);
+});
+
+// When you want to clear the chat history...
+document.querySelector("#clear-button").addEventListener("click", () => {
+  // clear the messages from the page
+  messages.innerHTML = "";
+  // clear the history from local storage
+  localStorage.removeItem("chatHistory");
 });
 
 socket.on("history", (history) => {
@@ -35,29 +67,6 @@ socket.on("history", (history) => {
     addMessage(message);
   });
 });
-
-function addMessage(message) {
-  messages.appendChild(
-    Object.assign(document.createElement("li"), { textContent: message })
-  );
-  messages.scrollTop = messages.scrollHeight;
-}
-
-// api connection
-const API_URL =
-  "https://www.rijksmuseum.nl/api/en/collection?key=9ZKSEiYs&involvedMaker=Johannes%20Vermeer";
-
-fetch(API_URL)
-  .then((response) => response.json())
-  .then((data) => {
-    // Get a random painting
-    const painting =
-      data.artObjects[Math.floor(Math.random() * data.artObjects.length)];
-    // Set the source of the image element to the image URL
-    const image = document.getElementById("painting-image");
-    image.src = painting.webImage.url;
-  })
-  .catch((error) => console.log(error));
 
 const params = new URLSearchParams(window.location.search);
 const user = params.get("user");
@@ -67,3 +76,21 @@ if (user) {
 } else {
   console.error("No user specified in URL");
 }
+
+socket.on("painting", (painting) => {
+  const image = document.getElementById("painting-image");
+  image.src = painting.webImage.url;
+});
+
+socket.on("correctGuess", (name) => {
+  // Change the background color to green for a few seconds
+  document.body.style.backgroundColor = "green";
+  setTimeout(() => {
+    document.body.style.backgroundColor = "";
+  }, 3000);
+
+  // Add a message to the chat indicating who guessed correctly
+  const correctGuessMessage = document.createElement("li");
+  correctGuessMessage.textContent = `${name} guessed the painting correctly!`;
+  messages.appendChild(correctGuessMessage);
+});
--- server.js
+++ server.js
@@ -4,6 +4,33 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const path = require("path");
 const port = process.env.PORT || 4242;
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+let currentPainting = null;
+let currentPaintingTitle = "";
+let connectedUsers = [];
+
+async function fetchAndBroadcastPainting() {
+  const API_URL =
+    "https://www.rijksmuseum.nl/api/en/collection?key=9ZKSEiYs&involvedMaker=Johannes%20Vermeer";
+
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  currentPainting =
+    data.artObjects[Math.floor(Math.random() * data.artObjects.length)];
+  currentPaintingTitle = currentPainting.title;
+  console.log(currentPaintingTitle);
+
+  // Broadcast the painting to all connected clients
+  io.emit("painting", currentPainting);
+}
+
+// Fetch the painting data immediately when the server starts
+fetchAndBroadcastPainting();
+
+// Fetch the painting data again every 5 minutes
+setInterval(fetchAndBroadcastPainting, 5 * 60 * 1000);
 
 // Start het longpolling proces, geef io mee
 // setInterval(callApi, 2500, io)
@@ -20,20 +47,46 @@ io.use((socket, next) => {
     return next(new Error("invalid nickname"));
   }
   socket.nickname = nickname;
+  connectedUsers.push(nickname);
+  socket.broadcast.emit("userConnected", nickname);
   next();
 });
 
-// SOCKET
-// CONNECTION
 io.on("connection", async (socket) => {
   console.log("a user connected", socket.nickname);
 
-  io.emit("userConnected", socket.nickname);
-});
+  socket.emit("history", history);
 
-function callApi(io) {
-  io.emit("whatever", "somebody set up us the bomb!");
-}
+  // In the message handler in io.on("connection"):
+  socket.on("message", (message) => {
+    message.user = socket.nickname;
+    history.push(message);
+    if (history.length > historySize) {
+      history.shift();
+    }
+
+    // Check if the message is the title of the current painting
+    if (message.text.toLowerCase() === currentPaintingTitle.toLowerCase()) {
+      // If it is, fetch and broadcast a new painting
+      fetchAndBroadcastPainting();
+
+      // Notify all clients who guessed the painting correctly
+      io.emit("correctGuess", socket.nickname);
+    } else {
+      // If not, simply broadcast the message as before
+      io.emit("message", message);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    connectedUsers = connectedUsers.filter((user) => user !== socket.nickname);
+    io.emit("connectedUsers", connectedUsers);
+  });
+
+  if (currentPainting) {
+    socket.emit("painting", currentPainting);
+  }
+});
 
 http.listen(port, () => {
   console.log("listening on http://localhost:" + port);
